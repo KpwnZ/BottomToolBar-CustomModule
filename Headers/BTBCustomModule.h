66fb1519eb3da5478eaa7b7327133e66ad4b1a7c
KpwnZ <Kpwn0@icloud.com>
2020-03-11 14:32:46 +0800

    Updated framework

--- Headers/BTBCustomModule.h
+++ Headers/BTBCustomModule.h
@@ -1,5 +1,1 @@
-// Umbrella header for BTBCustomModule.
-// Add import lines for each public header, like this: #import <BTBCustomModule/XXXAwesomeClass.h>
-// Don’t forget to also add them to BTBCustomModule_PUBLIC_HEADERS in your Makefile!
-
 #import "BTBCustomModuleViewController.h"
