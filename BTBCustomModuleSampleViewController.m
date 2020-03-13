28cc54685da25cd122010fe10362d6831cbd4f14
KpwnZ <Kpwn0@icloud.com>
2020-03-13 18:12:21 +0800

    Update description

--- BTBCustomModuleSampleViewController.m
+++ BTBCustomModuleSampleViewController.m
@@ -14,7 +14,7 @@
 
 	// Add your view to self.view
 	// if you want to change the corner radius, change the corner radius of self.containerView
-	// if you want to change the background or create more than one module in one row, you can add [self.containerView setHidden:YES] then create a background container by yourself
+	// if you want to change the background or create more than one module in one row, you can add self.containerView.backgroundColor = [UIColor clearColor] then create a background container by yourself
 
 	UILabel *label = [[UILabel alloc] initWithFrame:self.containerView.bounds];
 	label.text = @"BottomToolBar";
